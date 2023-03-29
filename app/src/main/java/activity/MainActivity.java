2d4cb2197008b4668091c7c72ac997b90b7b9fcd
Jackiany <jackiany14@gmail.com>
2023-03-28 21:04:39 -0300

    Componets de interface Fragments conversas e contatos criados Concliudo

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,16 +1,63 @@
 package activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fragments.R;
 
+import fragment.ConersasFragment;
+import fragment.ContatosFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnConversas, btnContatos;
+    private ConersasFragment conversasFragment;
+    private ContatosFragment contatosFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnContatos = findViewById(R.id.btnContatos);
+        btnConversas = findViewById(R.id.btnConversas);
+
+        getSupportActionBar().setElevation(0);
+
+        conversasFragment = new ConersasFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.FrameConteudo, conversasFragment);
+        transaction.commit();
+
+        btnContatos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                contatosFragment = new ContatosFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameConteudo, contatosFragment);
+                transaction.commit();
+
+            }
+        });
+
+        btnConversas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conversasFragment = new ConersasFragment();
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameConteudo, conversasFragment);
+                transaction.commit();
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/fragment/ContatosFragment.java
+++ app/src/main/java/fragment/ContatosFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.fragments.R;
 
@@ -16,6 +17,8 @@ import com.example.fragments.R;
  * create an instance of this fragment.
  */
 public class ContatosFragment extends Fragment {
+
+    private TextView txtContato;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +64,10 @@ public class ContatosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+        txtContato = view.findViewById(R.id.txtContato);
+        txtContato.setText("Contatos alterados");
+        return  view;
     }
+
 }
